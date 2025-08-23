93558f4ef56fe63dc03bd16908e9c3cec53b3e00
Clementine <clem.duc@proton.me>
2025-08-23 16:54:53 +0200

    add base component

---
+++ content/components/hurtbox_component.gd
@@ -0,0 +1,19 @@
+class_name HurtboxComponent
+extends Area2D
+
+@export var damage = 1.0
+
+signal hit_hitbox(hitbox)
+
+func _ready():
+	area_entered.connect(_on_hitbox_entered)
+
+func _on_hitbox_entered(hitbox: HitboxComponent):
+	# Make sure the area we are overlapping is a hurtbox
+	if not hitbox is HitboxComponent: return
+	# Make sure the hitbox isn't invincible
+	if hitbox.is_invincible: return
+	# Signal out that we hit a hitbox (this is useful for destroying projectiles when they hit something)
+	hit_hitbox.emit(hitbox)
+	# Have the hitbox signal out that it was hit
+	hitbox.hit.emit(self)
