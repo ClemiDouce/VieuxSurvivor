e235e04ced7806c89378631a9e9957baee38f674
Clementine <clem.duc@proton.me>
2025-08-23 16:55:03 +0200

    add base enemi and pidgeon

---
+++ content/gpe/entities/enemies/base_enemy.gd
@@ -0,0 +1,1 @@
+class_name BaseEnemy extends Node2D
---
+++ content/gpe/entities/enemies/pidgeon/pidgeon.gd
@@ -0,0 +1,19 @@
+class_name Pidgeon extends BaseEnemy
+
+@export var move_speed: float = 75.
+var target: Player
+var velocity := Vector2.ZERO
+
+func _ready() -> void:
+	target = get_tree().get_first_node_in_group("player")
+	
+func _physics_process(delta: float) -> void:
+	if target:
+		var direction = self.global_position.direction_to(target.global_position)
+		
+		translate(direction * move_speed * delta)
+		
+
+
+func _on_hurtbox_component_hit_hitbox(hitbox: Variant) -> void:
+	queue_free.call_deferred()
