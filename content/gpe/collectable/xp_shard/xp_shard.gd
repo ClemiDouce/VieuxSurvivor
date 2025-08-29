ca2b0dbda70251198aa815742f13567b5c3a3396
giitto <giitto92600@gmail.com>
2025-08-29 17:11:48 +0200

    xp shard has a more satifying flying pace

--- content/gpe/collectable/xp_shard/xp_shard.gd
+++ content/gpe/collectable/xp_shard/xp_shard.gd
@@ -9,6 +9,7 @@ func _physics_process(delta: float) -> void:
 		if self.global_position.distance_to(player.global_position) > 5. :
 			var direction = self.global_position.direction_to(player.global_position)
 			translate(direction * fly_speed * delta)
+			fly_speed *= 1.03
 		else:
 			player.gain_xp(xp_value)
 			queue_free.call_deferred()
